06ddcb9ab7b2003de788d9d0b3e62bc0e09b3901
catty3003 <catty_chan16@yahoo.com>
2014-01-20 18:12:46 +0100

    Erstes Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Shareflair::Application.config.secret_key_base = '8f66996854cf91f404c91f90a9d7f8e296ed614512d36af2e16eb9f0c272329858bc0013b9ac1cc91df31cc6e68ebb3cb91c0469a48e014188263c62a5ff6bd9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Shareflair::Application.config.session_store :cookie_store, key: '_shareflair_session'
