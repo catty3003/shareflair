a72fcbfe5d7a9a89b09b41bedab6efd3fe4b03c2
catty3003 <catty_chan16@yahoo.com>
2014-01-20 18:51:45 +0100

    Validationsregeln hinzugefuegt

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,6 @@
 class Product < ActiveRecord::Base
+	validates :title, presence: true
+	validates :category, presence: true
+	validates :target_group, presence: true
+	validates :size, presence: true
 end
