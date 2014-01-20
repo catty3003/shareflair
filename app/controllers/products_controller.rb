a763bfa4b73fcdf268d644abad4b2dfea503813f
catty3003 <catty_chan16@yahoo.com>
2014-01-20 22:42:04 +0100

    Zuerst erstellte Product zuerst gezeigt

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,6 +5,7 @@ class ProductsController < ApplicationController
   # GET /products.json
   def index
     @products = Product.all
+    @products = Product.order(created_at: :desc)
   end
 
   # GET /products/1
