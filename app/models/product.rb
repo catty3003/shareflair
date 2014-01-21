23ee70058ba60576d53201f97396a9c790597b92
catty3003 <catty_chan16@yahoo.com>
2014-01-21 15:10:57 +0100

    Task wird dem User zugeordnet, der diesen erstellt

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ActiveRecord::Base
+	belongs_to :user, class_name: "Task", foreign_key: "user_id"
 	validates :title, presence: true
 	validates :category, presence: true
 	validates :target_group, presence: true
