63f8c0cf89eb294d8a3202db70379d3ef2f5ca71
catty3003 <catty_chan16@yahoo.com>
2014-01-21 13:03:37 +0100

    bevor user devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,17 @@
 Shareflair::Application.routes.draw do
+  
   get "home/index"
   get "home/search"
   get "home/insert"
   get "home/user"
   get "home/about"
   resources :products
-
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'home#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
