adedfd39165a0556fdde75eb5632a49507a360cc
catty3003 <catty_chan16@yahoo.com>
2014-01-25 19:35:28 +0100

    available spalte zu Products hinzugefuegt

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140121151133) do
+ActiveRecord::Schema.define(version: 20140125182343) do
 
   create_table "categories", force: true do |t|
     t.string   "name"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20140121151133) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "image"
+    t.boolean  "active"
   end
 
   create_table "users", force: true do |t|
