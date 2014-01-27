f236b12572b03d1c1be2a32d407432c57758b454
catty3003 <catty_chan16@yahoo.com>
2014-01-27 15:13:36 +0100

    verlinkung user zu user profile und edit user moeglich

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,20 @@
 class UsersController < ApplicationController
-
+  before_action :set_user, only: [:profile]
+  
   def all_user
     @users =User.all
   end
 
 
   def profile
-    
+   
   end
   
 
-
+  private
+    def set_user
+       @user = User.find(params[:id])
+    end
 
   
 
